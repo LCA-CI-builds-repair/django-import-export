87e9f11f287c33778980d0ab57efa43d8d7a2a74
jovyan <jovyan@kovrigin-a100-80gb-0>
2024-04-07 21:39:34 +0000

    test_lca_code_editing__code-editing-metrics-xa7dde9937bcc934__id_87

--- import_export/admin.py
+++ import_export/admin.py
@@ -745,11 +745,12 @@ class ExportMixin(BaseExportMixin, ImportExportMixinBase):
         original_show_full_result_count = self.show_full_result_count
         self.show_full_result_count = False
 
-        class FakePaginator:
-            count = 0
-        original_get_paginator = self.get_paginator
-        self.get_paginator = lambda request, queryset, per_page: FakePaginator()
-        cl = ChangeList(**changelist_kwargs)
+    class FakePaginator:
+        count = 0
+
+    original_get_paginator = self.get_paginator
+    self.get_paginator = lambda request, queryset, per_page: FakePaginator()
+    cl = ChangeList(**changelist_kwargs)
         self.show_full_result_count = original_show_full_result_count
         self.get_paginator = original_get_paginator
 
