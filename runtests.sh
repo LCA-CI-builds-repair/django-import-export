e8781a60068a67a16d3f548ad76807e744e4f4ea
ubuntu <ubuntu@ip-10-0-0-135>
2024-05-02 12:05:47 +0000

    test_lca_code_editing__code-editing-metrics-x2a09a77c27775b8d__id_85

--- runtests.sh
+++ runtests.sh
@@ -5,13 +5,13 @@
 # sqlite (default) runs against local database file (database.db)
 # use pyenv or similar to install multiple python instances
 
-export DJANGO_SETTINGS_MODULE=settings
+export DJANGO_SETTINGS_MODULE="settings"
 
-export IMPORT_EXPORT_POSTGRESQL_USER=pguser
-export IMPORT_EXPORT_POSTGRESQL_PASSWORD=pguserpass
+export IMPORT_EXPORT_POSTGRESQL_USER="pguser"
+export IMPORT_EXPORT_POSTGRESQL_PASSWORD="pguserpass"
 
-export IMPORT_EXPORT_MYSQL_USER=mysqluser
-export IMPORT_EXPORT_MYSQL_PASSWORD=mysqluserpass
+export IMPORT_EXPORT_MYSQL_USER="mysqluser"
+export IMPORT_EXPORT_MYSQL_PASSWORD="mysqluserpass"
 
 echo "starting local database instances"
 docker compose -f tests/docker-compose.yml up -d
@@ -23,11 +23,11 @@ echo "running tests (sqlite)"
 tox
 
 echo "running tests (mysql)"
-export IMPORT_EXPORT_TEST_TYPE=mysql-innodb
+export IMPORT_EXPORT_TEST_TYPE="mysql-innodb"
 tox
 
 echo "running tests (postgres)"
-export IMPORT_EXPORT_TEST_TYPE=postgres
+export IMPORT_EXPORT_TEST_TYPE="postgres"
 tox
 
 echo "removing local database instances"
