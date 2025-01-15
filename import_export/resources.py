d7a245051410f7118c8344a830ee2898a07ed8bb
ubuntu <ubuntu@ip-10-0-1-33>
2025-01-15 16:40:34 +0000

    waleko__claude-3.5-sonnet_d261__id_85

--- import_export/resources.py
+++ import_export/resources.py
@@ -860,8 +860,8 @@ class Resource(metaclass=DeclarativeMetaclass):
 
         :param row: A ``dict`` containing key / value data for the row to be imported.
 
-        :param row_result: A ``RowResult`` instance.
-          References the persisted ``instance`` as an attribute.
+        :param row_result: A ``RowResult`` instance. References information about
+          the persisted row.instance`` as an attribute.
 
         :param \**kwargs:
             See :meth:`import_row`
