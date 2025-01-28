9d8c4ba35681fd1f3b54760e5da6097958476399
ubuntu <ubuntu@ip-10-0-1-6>
2025-01-28 17:01:12 +0000

    waleko__llama-3.1-70b_a8a8__id_84

--- import_export/admin.py
+++ import_export/admin.py
@@ -259,7 +259,9 @@ class ImportMixin(BaseImportMixin, ImportExportMixinBase):
         messages.success(request, success_message)
 
     def get_import_context_data(self, **kwargs):
-        return self.get_context_data(**kwargs)
+        return self.get_context_data(
+        **kwargs
+    )
 
     def get_context_data(self, **kwargs):
         return {}
