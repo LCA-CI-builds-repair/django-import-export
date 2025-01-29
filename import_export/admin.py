b145872c5b336ee242cd1004f421613b6bef6592
ubuntu <ubuntu@ip-10-0-1-55>
2025-01-29 16:01:04 +0000

    waleko__deepseek-r1_78ab__id_84

--- import_export/admin.py
+++ import_export/admin.py
@@ -248,8 +248,15 @@ class ImportMixin(BaseImportMixin, ImportExportMixinBase):
     def add_success_message(self, result, request):
         opts = self.model._meta
 
+        success_message = _("Import finished, with {} new and {} updated {}.").format(
+            result.totals[RowResult.IMPORT_TYPE_NEW],
+            result.totals[RowResult.IMPORT_TYPE_UPDATE],
+            opts.verbose_name_plural,
+        )
+
         success_message = _(
-            "Import finished, with {} new and " "{} updated {}."
+            "Import finished, with {} new and "
+            "{} updated {}."
         ).format(
             result.totals[RowResult.IMPORT_TYPE_NEW],
             result.totals[RowResult.IMPORT_TYPE_UPDATE],
