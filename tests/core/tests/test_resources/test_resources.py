f529e894516e3e6f0d7a32f41950b8586e8bd6db
ubuntu <ubuntu@ip-10-0-1-14>
2025-01-29 19:25:15 +0000

    waleko__claude-3-opus_7136__id_86

--- tests/core/tests/test_resources/test_resources.py
+++ tests/core/tests/test_resources/test_resources.py
@@ -5,7 +5,6 @@ from copy import deepcopy
 from datetime import date
 from decimal import Decimal, InvalidOperation
 from unittest import mock, skipUnless
-from unittest.mock import patch
 
 import tablib
 from core.models import (
